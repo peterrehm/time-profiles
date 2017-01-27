f1345372b4a96110fe12a9b96d9d1658c45f3220
Peter Rehm <peter.rehm@renvest.de>
2017-01-27 10:50:15 +0100

    Enabled phpmd and updated vendors

--- bin/run_static_analysis.sh
+++ bin/run_static_analysis.sh
@@ -4,3 +4,4 @@ mkdir -p $DIR/../build
 cd $DIR/..
 $DIR/phpcs --standard=phpcs.xml $DIR/../src/ --extensions=php --report=checkstyle --report-file=$DIR/../build/phpcs.xml --ignore=Tests
 $DIR/phpcpd --names="*.php" $DIR/../src/ --log-pmd=$DIR/../build/pmd-cpd.xml --exclude=Tests
+$DIR/phpmd $DIR/../src/ xml codesize,unusedcode --reportfile $DIR/../build/pmd.xml --exclude Tests

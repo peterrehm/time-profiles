5aaf06093f12b88a00a59f491cd4b0b8e6f21183
Peter Rehm <peter.rehm@renvest.de>
2017-02-09 10:36:12 +0100

    Updated test config

--- bin/run_static_analysis.sh
+++ bin/run_static_analysis.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p $DIR/../build
-cd $DIR/..
-$DIR/phpcs --standard=phpcs.xml $DIR/../src/ --extensions=php --report=checkstyle --report-file=$DIR/../build/phpcs.xml --ignore=Tests
-$DIR/phpcpd --names="*.php" $DIR/../src/ --log-pmd=$DIR/../build/pmd-cpd.xml --exclude=Tests
-$DIR/phpmd $DIR/../src/ xml codesize,unusedcode --reportfile $DIR/../build/pmd.xml --exclude Tests
+vendor/peterrehm/php-test/bin/run_static_analysis.sh
